f9d3e48af81200d68f259d87c80dfc80c807902b
Oded Arbel <oded@geek.co.il>
2025-09-14 17:39:19 +0000

    no need for sample anymore

--- app/db_config.sample.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Copy this file to db_config.php and update the values for your environment.
-return [
-    'host' => 'localhost',
-    'dbname' => 'comix',
-    'user' => 'comixuser',
-    'password' => 'your_password_here',
-];
